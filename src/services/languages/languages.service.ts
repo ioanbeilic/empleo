2df6044bb1b7b91fc4f9d357f9650326178d8c0c
Ioan Beilic <ioan.beilic@tecdata.es>
2019-08-01 17:05:48 +0200

    feat(languages): documentation

--- src/services/languages/languages.service.ts
+++ src/services/languages/languages.service.ts
@@ -10,11 +10,21 @@ import { CvService } from '../cv/cv.service';
 
 @Injectable()
 export class LanguagesService {
+  /**
+   *
+   * @param languageRepository - repository for entity language
+   * @param cvService - dependency injection to create a new cv if not exist with method  ensureExists
+   */
   constructor(
     @InjectRepository(Language) private readonly languageRepository: Repository<Language>,
     private readonly cvService: CvService
   ) {}
 
+  /**
+   * create/add new language to a cv
+   * If cv don`t exist is created
+   * @param { user, language: languageCreate } where user is loggedIn user ans language is form data from front side
+   */
   async createLanguage({ user, language: languageCreate }: CreateLanguageOptions): Promise<Language> {
     const language = this.languageRepository.create({
       ...languageCreate,
@@ -27,6 +37,10 @@ export class LanguagesService {
     return this.languageRepository.save(language);
   }
 
+  /**
+   * find a specifica language by id
+   * @param { languageId, user } - language id and logged user
+   */
   async findUserLanguageById({ languageId, user }: { languageId: LanguageId; user: User }): Promise<Language> {
     const language = await this.languageRepository.findOne({ languageId, keycloakId: user.id });
 
@@ -37,10 +51,19 @@ export class LanguagesService {
     return language;
   }
 
+  /**
+   * update a existing language
+   * @param { languageId, user } - language id and logged user
+   */
   async updateOne({ language, update }: UpdateLanguageOptions): Promise<void> {
     await this.languageRepository.update({ languageId: language.languageId }, update);
   }
 
+  /**
+   * delete language if the language belong to the user
+   * @param { languageId, user } - language id and logged user
+   * return 404 language not found if the language not exist or don`t belong to the user
+   */
   async deleteOne({ user, languageId }: DeleteLanguageOptions) {
     const { affected } = await this.languageRepository.delete({ languageId, keycloakId: user.id });
 
