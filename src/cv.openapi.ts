5189658ae0f496fd8819710d84278d862643baea
Lorens León <lorens.leon@gruposantander.com>
2019-08-01 16:27:52 +0200

    chore: add oauth 2 login to swagger

--- src/cv.openapi.ts
+++ src/cv.openapi.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { DocumentBuilder, SwaggerBaseConfig } from '@nestjs/swagger';
+import { KEYCLOAK_AUTHORIZATION_URI, KEYCLOAK_TOKEN_URI } from 'empleo-nestjs-authentication';
 import { CvConfigurationService } from './configuration/cv-configuration.service';
 
 @Injectable()
 export class CvOpenapi {
-  constructor(private readonly configurationService: CvConfigurationService) {}
+  constructor(
+    private readonly configurationService: CvConfigurationService,
+    @Inject(KEYCLOAK_AUTHORIZATION_URI) private readonly authUri: string,
+    @Inject(KEYCLOAK_TOKEN_URI) private readonly tokenUri: string
+  ) {}
 
   getDocument(): SwaggerBaseConfig {
     // This "really clever" patch is needed in order to make the api explorer usable when the app is deployed through api manager
@@ -17,6 +22,7 @@ export class CvOpenapi {
       .setDescription('Empleo micro service for managing CV')
       .setVersion(this.configurationService.getPackageVersion())
       .addTag('cv')
+      .addOAuth2('password', this.authUri, this.tokenUri)
       .addBearerAuth()
       .setSchemes(...schemes)
       .setBasePath(basePath)
